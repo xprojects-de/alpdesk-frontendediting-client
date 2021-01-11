07ff5c6b2b89bcd340a1b275c6f50ff85171a057
XProjects B.Hummel <infos@x-projects.de>
2021-01-11 19:11:51 +0100

    Migrate Repo

---
+++ src/app/items/item-move/item-move.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BaseItemComponent } from '../base-item/base-item.component';
+
+@Component({
+  selector: 'app-item-move',
+  templateUrl: './item-move.component.html',
+  styleUrls: ['./item-move.component.scss']
+})
+export class ItemMoveComponent extends BaseItemComponent {
+
+}
