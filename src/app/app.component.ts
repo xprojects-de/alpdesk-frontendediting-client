14dc5626b14ccac0cfd7b192e121d256546e108b
XProjects B.Hummel <infos@x-projects.de>
2021-02-04 15:43:56 +0100

    remove overlay background-color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
   // Just for Testing - Will be as Input from Component
   @Input('base') base: string = 'https://contao.local:8890/';
-  @Input('rt') rt: string = 'ZHKh_YzYjqtpzAthJOdDzbVy9WbhCjNM8wnQCb7T4ls';
+  @Input('rt') rt: string = 'R2m_s_tkqsn-zFho-A-QmFznN9s1aHW0xvsRnkSoYd8';
   @Input('frameurl') frameurl: string = '/preview.php';
 
   @HostListener('document:' + Constants.ALPDESK_EVENTNAME, ['$event']) onAFEE_Event(event: CustomEvent) {
