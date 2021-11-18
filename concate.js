f10a83d0d3e8bcb119a4f20b52bb88be60383328
Benjamin Hummel <55982921+xprojects-de@users.noreply.github.com>
2021-11-18 05:23:44 +0100

    update angular step 5

--- concate.js
+++ concate.js
@@ -1,25 +1,18 @@
 const fs = require('fs-extra');
 const concat = require('concat');
 (async function build() {
-    const files_es5 = [
+    const files = [
         './mootoolscompat.js',
-        './dist/alpdeskfee-client/runtime-es5.js',
-        './dist/alpdeskfee-client/polyfills-es5.js',
-        './dist/alpdeskfee-client/main-es5.js',
-    ]
-    const files_es6 = [
-        './mootoolscompat.js',
-        './dist/alpdeskfee-client/runtime-es2015.js',
-        './dist/alpdeskfee-client/polyfills-es2015.js',
-        './dist/alpdeskfee-client/main-es2015.js',
+        './dist/alpdeskfee-client/runtime.js',
+        './dist/alpdeskfee-client/polyfills.js',
+        './dist/alpdeskfee-client/main.js',
     ]
     const files_css = [
         './contao.css',
         './dist/alpdeskfee-client/styles.css',
     ]
     await fs.ensureDir('elements')
-    await concat(files_es5, 'elements/alpdeskfee-elements-es5.js');
-    await concat(files_es6, 'elements/alpdeskfee-elements-es6.js');
+    await concat(files, 'elements/alpdeskfee-elements.js');
     await concat(files_css, 'elements/alpdeskfee-styles.css');
     //await fs.copyFile('./dist/alpdeskfee-client/styles.css', 'elements/alpdeskfee-styles.css')
     //await fs.copy('./dist/angular-elements/assets/', 'elements/assets/')
--- concatedev.js
+++ concatedev.js
@@ -1,27 +1,19 @@
 const fs = require('fs-extra');
 const concat = require('concat');
 (async function build() {
-    const files_es5 = [
+    const files = [
         './mootoolscompat.js',
-        './dist/alpdeskfee-client/runtime-es5.js',
-        './dist/alpdeskfee-client/polyfills-es5.js',
-        './dist/alpdeskfee-client/vendor-es5.js',
-        './dist/alpdeskfee-client/main-es5.js',
-    ]
-    const files_es6 = [
-        './mootoolscompat.js',
-        './dist/alpdeskfee-client/runtime-es2015.js',
-        './dist/alpdeskfee-client/polyfills-es2015.js',
-        './dist/alpdeskfee-client/vendor-es2015.js',
-        './dist/alpdeskfee-client/main-es2015.js',
+        './dist/alpdeskfee-client/runtime.js',
+        './dist/alpdeskfee-client/polyfills.js',
+        './dist/alpdeskfee-client/vendor.js',
+        './dist/alpdeskfee-client/main.js',
     ]
     const files_css = [
         './contao.css',
         './dist/alpdeskfee-client/styles.css',
     ]
     await fs.ensureDir('elements')
-    await concat(files_es5, 'elements/alpdeskfee-elements-dev-es5.js');
-    await concat(files_es6, 'elements/alpdeskfee-elements-dev-es6.js');
+    await concat(files, 'elements/alpdeskfee-elements-dev.js');
     await concat(files_css, 'elements/alpdeskfee-styles-dev.css');
     //await fs.copyFile('./dist/alpdeskfee-client/styles.css', 'elements/styles-dev.css')
     //await fs.copy('./dist/angular-elements/assets/', 'elements/assets/')
